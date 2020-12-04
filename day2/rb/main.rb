55e88b5049041d7a922ad3d19c9dd839feaeae68
eenblam <ben.a.elam@gmail.com>
2020-12-04 13:51:45 -0800

    day 2 part 2

--- day2/rb/main.rb
+++ day2/rb/main.rb
@@ -6,4 +6,15 @@ def validate_record(r)
 	(min <= n) and (n <= max)
 end
 
-puts IO.readlines('./input', chomp: true).select{|r| validate_record(r)}.count
+def validate_record_by_index(r)
+	policy, password = r.split(': ')
+	indices, target = policy.split
+	# Decrement index by 1 to account for 0-indexing
+	i, j = indices.split('-').map {|x| x.to_i - 1}
+	first, second = password[i] == target, password[j] == target
+	(first or second) and not (first and second)
+end
+
+data = IO.readlines('./input', chomp: true)
+puts data.select{|r| validate_record(r)}.count
+puts data.select{|r| validate_record_by_index(r)}.count
