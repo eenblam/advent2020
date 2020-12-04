6881efb301e33c96afcdcb9a19622ff496e22eea
Ben Elam <benelam@eff.org>
2020-12-04 13:09:58 -0800

    README.md + day1

---
+++ day1/rb/main.rb
@@ -0,0 +1,20 @@
+require 'set'
+
+nums = IO.readlines('./input', chomp: true).map { |s| s.to_i }
+ds = Set[]
+
+# Both solutions assume only one result (identifies first)
+
+# Product of two inputs summing to 2020
+nums.each do |num|
+		diff = 2020 - num
+		if ds.include?(num)
+			puts num * diff
+			break
+		end
+		ds.add(diff)
+end
+
+# Product of three inputs summing to 2020
+# There's probably a more optimized algorithm, but...
+puts nums.combination(3).select {|x,y,z| x+y+z == 2020}[0].reduce {|x,y| x*y}
