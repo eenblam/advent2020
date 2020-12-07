08e3534d7dfcc17a0125108964a8dfb72192e1fc
eenblam <ben.a.elam@gmail.com>
2020-12-06 16:15:12 -0800

    Solve day 6 part 2
    Locked ruby version via rbenv

--- day6/rb/main.rb
+++ day6/rb/main.rb
@@ -1,10 +1,19 @@
-def run(filename)
+def count_union(filename)
   IO.read(filename)
     .split(%r{\n\n})
     .map{ |x| x.gsub(/\s+/, "").chars.uniq.count }
     .sum
 end
 
-raise "Part 1 test fail" unless run('./testdata') == 11
+def count_intersection(filename)
+  IO.read(filename)
+    .split(%r{\n\n})
+    .map{ |x| x.split(%r{\n}).map(&:chars).reduce(&:intersection).count }
+    .sum
+end
 
-puts run('./input')
+raise "Part 1 test fail" unless count_union('./testdata') == 11
+raise "Part 2 test fail" unless count_intersection('./testdata') == 6
+
+puts count_union('./input')
+puts count_intersection('./input')
